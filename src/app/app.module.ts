a2742b4be2fbb7b0a3e0cc74a4d17ab73a632174
Igaro-Franklin <igarofranklin@gmail.com>
2024-10-23 07:45:21 -0300

    Enviando arquivo inicial do projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [],
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ColaboradoresComponent } from './colaboradores/colaboradores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'colaboradores', component: ColaboradoresComponent },
+
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+
+];
