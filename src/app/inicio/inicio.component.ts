a07da6c9129788818a9cb1644ea4556d7efbea65
Igaro-Franklin <igarofranklin@gmail.com>
2024-11-28 06:58:45 -0300

    Mudou o componente de lembrete, fazendo apenas a parte do front

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,31 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [],
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent{
-  novoLembrete: String = '';
-  lembrentes: String[] = [];
 
   constructor(){
-    const alertasSalvos = localStorage.getItem('meusLembretes');
-    if(alertasSalvos){
-      this.lembrentes = JSON.parse(alertasSalvos);
-    }
   }
   
-  adicionarLembrete(){
-    if(this.novoLembrete.trim() !== ''){
-      this.lembrentes.push(this.novoLembrete);
-      localStorage.setItem('meusLembretes', JSON.stringify(this.lembrentes));
-      this.novoLembrete = '';
-    }
-  }
+  adicionarLembrete(){}
 
 }
